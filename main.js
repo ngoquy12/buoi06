fcef4f853ae79c700d6d0fedade57edefb9aff33
quynv <quynv.htosoftware@gmail.com>
2026-04-01 13:03:04 +0700

    update files

--- main.js
+++ main.js
@@ -5,11 +5,19 @@ const emailInputElement = document.getElementById("email");
 const ageInputElement = document.getElementById("age");
 const btnAddElement = document.getElementById("btnAdd");
 const tbodyElement = document.getElementById("tableBody");
+const titleFormElement = document.getElementById("title-form");
+const modalConfirmElement = document.getElementById("modal-confirm");
+const iconCloseElement = document.getElementById("icon-close");
+const btnCancelElement = document.getElementById("btn-cancel");
+const btnConfirmedElement = document.getElementById("btn-confirmed");
 
 // Các phần tử liên quan đến lỗi
 const errorNameElement = document.getElementById("errorName");
 const errorEmailElement = document.getElementById("errorEmail");
 const errorAgeElement = document.getElementById("errorAge");
+
+let flag = null; // Cờ lấy thông tin cần cập nhật
+let indexDelete = null; // Lấy index cần xóa
 
 const validateEmail = (email) => {
   return String(email)
@@ -34,6 +42,9 @@ const renderStudents = () => {
             <td>${element.age}</td>
             <td>
                 <button onclick="handleDelete(${index})">Xóa</button>
+            </td>
+            <td>
+                <button onclick="handleEdit(${index})">Sửa</button>
             </td>
         </tr>
    `;
@@ -85,16 +96,28 @@ btnAddElement.addEventListener("click", () => {
 
   // 4. Kiểm tra xem các điều kiện đã thỏa mãn chưa?
   if (inValid) {
-    // 5. Gom các dữ liệu từ Form thành 1 đối tượng
-    const newStudent = {
-      id: Math.ceil(Math.random() * 100000000),
-      name: fullNameInputElement.value,
-      email: emailInputElement.value,
-      age: ageInputElement.value,
-    };
+    if (!flag && flag !== 0) {
+      // 5. Gom các dữ liệu từ Form thành 1 đối tượng
+      const newStudent = {
+        id: Math.ceil(Math.random() * 100000000),
+        name: fullNameInputElement.value,
+        email: emailInputElement.value,
+        age: ageInputElement.value,
+      };
 
-    // 6. Thêm dữ liệu vào trong mảng
-    students.unshift(newStudent);
+      // 6. Thêm dữ liệu vào trong mảng
+      students.unshift(newStudent);
+    } else {
+      // Cập nhật lại dữ liệu cho đối tượng bằng các giá trị mới
+      // Truy cập vào phần tử của mảng theo vị trí (flag) để gán lại giá trị
+      students[flag].name = fullNameInputElement.value;
+      students[flag].email = emailInputElement.value;
+      students[flag].age = ageInputElement.value;
+
+      // Cập nhật lại tiêu đề
+      titleFormElement.textContent = "Thêm mới sinh viên";
+      btnAddElement.textContent = "Thêm sinh viên";
+    }
 
     // 7. Lưu dữ liệu lên local
     localStorage.setItem("students", JSON.stringify(students));
@@ -109,21 +132,55 @@ btnAddElement.addEventListener("click", () => {
   }
 });
 
+// Hàm đóng modal xác nhận xóa
+const handleCloseModal = () => {
+  modalConfirmElement.style.display = "none";
+};
+
+// Bắt sự kiện khi nhấn vào icon close, sẽ ẩn modal xác nhận xóa
+iconCloseElement.addEventListener("click", () => {
+  handleCloseModal();
+});
+
+// Bắt sự kiện khi nhấn vào nút hủy trong modal, sẽ ẩn modal xác nhận xóa
+btnCancelElement.addEventListener("click", () => {
+  handleCloseModal();
+});
+
+// Bắt sự kiện người dùng nhấn vào nút xác nhận trong modal xóa
+btnConfirmedElement.addEventListener("click", () => {
+  // Xóa phần tử khỏi mảng (splice)
+  students.splice(indexDelete, 1);
+  // Lưu dữ liệu lên local
+  localStorage.setItem("students", JSON.stringify(students));
+  // Render lại dữ liệu mới nhất
+  renderStudents();
+  // Đóng modal xác nhận xóa
+  handleCloseModal();
+});
+
 // Hàm xóa sinh viên
 const handleDelete = (index) => {
   // Mở modal xác nhận xóa
-  const isConfirm = confirm(
-    `Bạn có chắc chắn muốn xóa sinh viên có tên là ${students[index].name} không?`,
-  );
+  modalConfirmElement.style.display = "flex";
+};
 
-  if (isConfirm) {
-    // Xóa phần tử khỏi mảng (splice)
-    students.splice(index, 1);
+// Hàm sửa thông tin sinh viên
+const handleEdit = (index) => {
+  // Thay đổi tiêu đề của form
+  titleFormElement.textContent = "Cập nhật sinh viên";
 
-    // Lưu dữ liệu lên local
-    localStorage.setItem("students", JSON.stringify(students));
+  // Thay đổi tiêu đề của button
+  btnAddElement.textContent = "Cập nhật";
 
-    // Render lại dữ liệu mới nhất
-    renderStudents();
-  }
+  // Fill dữ liệu của sinh viên vào trong Form
+  // Thông qua vị trí, lấy ra phần tử của mảng: ten_mang[vi_tri]
+
+  // Cập nhật giá trị vừa từ mảng và cập nhật cho từng input
+  fullNameInputElement.value = students[index].name;
+  emailInputElement.value = students[index].email;
+  ageInputElement.value = students[index].age;
+
+  // Cập nhật flag để biết đây là hành động sửa
+  flag = index; // gán lại để lấy ra giá trị vị trí cần chỉnh sửa
 };
